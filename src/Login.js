912f42eb4dd2db38c1020188439ea0109b47ad93
Nikhil Hegde <nikhil.hegde@ymedialabs.com>
2019-10-17 11:37:48 +0530

    Changes

--- src/Login.js
+++ src/Login.js
@@ -9,15 +9,15 @@ class Login extends Component {
     this.login = this.login.bind(this);
     this.signup = this.signup.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    this.signUp=this.signUp.bind(this);
-    this.signIn=this.signIn.bind(this);
+    this.signUp = this.signUp.bind(this);
+    this.signIn = this.signIn.bind(this);
     this.state = {
       email: '',
-      confirmEmail:'',
+      confirmEmail: '',
       password: '',
-      confirmPassword:'',
-      flag:0,
-      username:null
+      confirmPassword: '',
+      flag: 0,
+      username: null
     };
   }
 
@@ -28,96 +28,92 @@ class Login extends Component {
   login(e) {
     console.log("login");
     e.preventDefault();
-    fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
-        // this.props.history.push("/");
-      })
+    fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u) => {
+      this.props.history.push("/");
+    })
       .catch((error) => {
         console.log(error);
         alert("Invalid Email or Password");
       });
   }
-  signup(e){
+  signup(e) {
     e.preventDefault();
-    if((this.state.password==this.state.confirmPassword)&&(this.state.email==this.state.confirmEmail)){
+    if ((this.state.password == this.state.confirmPassword) && (this.state.email == this.state.confirmEmail)) {
       fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .catch((error) => {
-        alert("Invalid Email or Password");
-      })
-    }
-      else{
+        .catch((error) => {
           alert("Invalid Email or Password");
-      }
+        })
     }
-
-signUp(){
-  this.setState({ flag: 1 });
-  console.log(this.state.flag);
-  <script>
-    {document.getElementById("signUp").style.borderBottom="2px solid black"}
-   {document.getElementById("signIn").style.borderBottom="none"}
-  </script>
-  
-}
-signIn(){
-  this.setState({ flag: 0 });
-  console.log(this.state.flag);
-  <script>
-    {document.getElementById("signIn").style.borderBottom="2px solid black"}
-    {document.getElementById("signUp").style.borderBottom="none"}
-  </script>
-}
-
-onFacebookLogin = (loginStatus, resultObject) => {
-  if (loginStatus === true) {
-    this.setState({
-      username: resultObject.user.name
-    });
-  } else {
-    alert('Facebook login error');
+    else {
+      alert("Invalid Email or Password");
+    }
   }
-}
+
+  signUp() {
+    this.setState({ flag: 1 });
+    console.log(this.state.flag);
+    document.getElementById("signUp").style.borderBottom = "2px solid black";
+    document.getElementById("signIn").style.borderBottom = "none";
+
+  }
+  signIn() {
+    this.setState({ flag: 0 });
+    console.log(this.state.flag);
+    document.getElementById("signIn").style.borderBottom = "2px solid black";
+    document.getElementById("signUp").style.borderBottom = "none";
+  }
+
+  onFacebookLogin = (loginStatus, resultObject) => {
+    if (loginStatus === true) {
+      this.setState({
+        username: resultObject.user.name
+      });
+    } else {
+      alert('Facebook login error');
+    }
+  }
 
   render() {
     return (
-<Fragment>
-  <div className="login">
-      <header className="status-bar">
-          <div className="login-Signup">
-           <nav id="signIn" onClick={this.signIn}>SIGN IN</nav>
-           <nav id="signUp" onClick={this.signUp}>SIGN UP</nav>
-         </div>
-       </header>
-       {this.state.flag?(<div className="signUpContainer">
-                      <div className="signUpContent">
-                         <input id="firstNamefield" type="text" placeholder="First Name"/>
-                         <input id="lastNamefield" type="text" placeholder="Last Name"/>
-                         <input id="emailIdfield" value={this.state.email} onChange={this.handleChange} type="email" name="email" aria-describedby="emailHelp" placeholder="Email Address"/>
-                         <input id="confirmEmailIdfield" value={this.state.confirmEmail} onChange={this.handleChange} type="email" name="confirmEmail" aria-describedby="emailHelp" placeholder="Confirm Email Address"/>
-                         <input id="newPasswordfield" type="password" value={this.state.password} onChange={this.handleChange} name="password" placeholder="Password"/>
-                         <input id="confirmNewPasswordfield" value={this.state.confirmPassword} onChange={this.handleChange} type="password" name="confirmPassword"  placeholder="Confirm Password"/>
-                         <button id="createBtn" onClick={this.signup}>CREATE</button>
-                         <button id="facebookLoginBtn">SIGN IN WITH FACEBOOK</button>
-                         <span id="footerfield">View our Privacy Policy for more details.</span>
-                      </div>
-                 </div>):(<div className="content">
-                                  <section className="signInCard">
-                                    <div className="shoppingWorldicon"></div>
-                                    <div className="shoppingworldtitle">
-                                        <p>Shopping</p>
-                                       <p>World</p>
-                                    </div>
-                                     <input  id="mailIdfield" value={this.state.email} onChange={this.handleChange} type="email" name="email" aria-describedby="emailHelp" placeholder="Email Address"/>
-                                    <input id="passwordfield" value={this.state.password} onChange={this.handleChange} type="password" name="password" placeholder="Password"/>
-                                    <button id="loginBtn" onClick={this.login}>LOGIN</button>
-                                    <span>Forgot Password?</span>
-                                    <FacebookLogin onLogin={this.onFacebookLogin}>
-                                    <button id="facebookLoginBtn" >SIGN IN WITH FACEBOOK</button>
-                                    </FacebookLogin>
-                                    <span id="footerfield">View our Privacy Policy for more details.</span>
-                                  </section>
-                                </div>)}
- </div> 
-</Fragment>
+      <Fragment>
+        <div className="login">
+          <header className="status-bar">
+            <div className="login-Signup">
+              <nav id="signIn" onClick={this.signIn}>SIGN IN</nav>
+              <nav id="signUp" onClick={this.signUp}>SIGN UP</nav>
+            </div>
+          </header>
+          {this.state.flag ? (<div className="signUpContainer">
+            <div className="signUpContent">
+              <input id="firstNamefield" type="text" placeholder="First Name" />
+              <input id="lastNamefield" type="text" placeholder="Last Name" />
+              <input id="emailIdfield" value={this.state.email} onChange={this.handleChange} type="email" name="email" aria-describedby="emailHelp" placeholder="Email Address" />
+              <input id="confirmEmailIdfield" value={this.state.confirmEmail} onChange={this.handleChange} type="email" name="confirmEmail" aria-describedby="emailHelp" placeholder="Confirm Email Address" />
+              <input id="newPasswordfield" type="password" value={this.state.password} onChange={this.handleChange} name="password" placeholder="Password" />
+              <input id="confirmNewPasswordfield" value={this.state.confirmPassword} onChange={this.handleChange} type="password" name="confirmPassword" placeholder="Confirm Password" />
+              <button id="createBtn" onClick={this.signup}>CREATE</button>
+              <button id="facebookLoginBtn">SIGN IN WITH FACEBOOK</button>
+              <span id="footerfield">View our Privacy Policy for more details.</span>
+            </div>
+          </div>) : (<div className="content">
+            <section className="signInCard">
+              <div className="shoppingWorldicon"></div>
+              <div className="shoppingworldtitle">
+                <p>Shopping</p>
+                <p>World</p>
+              </div>
+              <input id="mailIdfield" value={this.state.email} onChange={this.handleChange} type="email" name="email" aria-describedby="emailHelp" placeholder="Email Address" />
+              <input id="passwordfield" value={this.state.password} onChange={this.handleChange} type="password" name="password" placeholder="Password" />
+              <button id="loginBtn" onClick={this.login}>LOGIN</button>
+              <span>Forgot Password?</span>
+              <FacebookLogin onLogin={this.onFacebookLogin}>
+                <button id="facebookLoginBtn" >SIGN IN WITH FACEBOOK</button>
+              </FacebookLogin>
+              <span id="footerfield">View our Privacy Policy for more details.</span>
+            </section>
+          </div>)}
+        </div>
+      </Fragment>
     );
   }
 }
--- src/ShoppingAr.js
+++ src/ShoppingAr.js
@@ -5,79 +5,73 @@ import shirtMtl from './3dmodels/Tshirt1/BetterTShirt/BetterShirt.mtl';
 
 class Areality extends Component {
     constructor() {
-      super();
-      this.flag=0,this.scalex=0,this.scaley=0.5,this.scalez=0.5,this.posz=-1,
-      this.$= document.getElementById.bind(document),
-      this.arDecr=this.$("arSizeDecrement")
-    } 
-            setAttributes=(key,value) => {
-                var arObj=this.$("arObject");
-                arObj.setAttribute(key,value);
-            }
-            removeAttributes=(key) => {
-                var arObj=this.$("arObject");
-                arObj.removeAttribute(key);
-            }
-        increase=()=>{
-            this.scalex += 0.5;
-            this.scaley += 0.5;
-            this.scalez=1.5;
-            this.posz+=2.2;
+        super();
+        this.flag = 0, this.scalex = 0, this.scaley = 0.5, this.scalez = 0.5, this.posz = -1,
+            this.$ = document.getElementById.bind(document),
+            this.arDecr = this.$("arSizeDecrement")
+    }
+    setAttributes = (key, value) => {
+        var arObj = this.$("arObject");
+        arObj.setAttribute(key, value);
+    }
+    removeAttributes = (key) => {
+        var arObj = this.$("arObject");
+        arObj.removeAttribute(key);
+    }
+    increase = () => {
+        this.scalex += 0.5;
+        this.scaley += 0.5;
+        this.scalez = 1.5;
+        this.posz += 2.2;
 
-            if (this.scalex > 4) {
-                this.scalex = this.scaley=0.5;
-                this.posz=-1;
-            }
-            console.log("scale:");
-            console.log(''+ this.scalex +'' + this.scaley + '' + this.scalez + '');
-            this.setAttributes('scale', ''+ this.scalex +' ' + this.scaley + ' ' + this.scalez + '');
-            this.setAttributes('position', '0 0 '+this.posz+'');
-            console.log("position:");
-            console.log( '0 0 '+this.posz+'');
+        if (this.scalex > 4) {
+            this.scalex = this.scaley = 0.5;
+            this.posz = -1;
         }
-        decrease=()=>{
-            this.scalex -= 0.5;
-            this.scaley -= 0.5;
-            this.scalez=1.5;
-            this.posz-=2.2;
-            if (this.scalex <= 0) {
-                this.scalex = this.scaley=this.scalez=0.5;
-                this.posz=-1;
-            }
-            console.log('' + this.scalex + ' ' + this.scaley + ' ' + this.scalez + '');
-            this.setAttributes('scale', '' + this.scalex+ ' ' + this.scaley + ' ' + this.scalez + '');
-            this.setAttributes('position', '0 0 '+this.posz+'');
+        this.setAttributes('scale', '' + this.scalex + ' ' + this.scaley + ' ' + this.scalez + '');
+        this.setAttributes('position', '0 0 ' + this.posz + '');
+    }
+    decrease = () => {
+        this.scalex -= 0.5;
+        this.scaley -= 0.5;
+        this.scalez = 1.5;
+        this.posz -= 2.2;
+        if (this.scalex <= 0) {
+            this.scalex = this.scaley = this.scalez = 0.5;
+            this.posz = -1;
         }
+        this.setAttributes('scale', '' + this.scalex + ' ' + this.scaley + ' ' + this.scalez + '');
+        this.setAttributes('position', '0 0 ' + this.posz + '');
+    }
 
     render() {
-        console.log(shirtMtl,shirtObj);
         return (
-         <Fragment>
-             <div className="shoppingWorldAr">
-             <a href ="/Men" exact="true">
-                    <button className="arButtonScreen" id="arBackBtn" >Back</button>
-              </a>
-             <button  className="arButtonScreen" id="arSizeIncrement" onClick={this.increase}>+</button>
-             <button  className="arButtonScreen" id="arSizeDecrement" onClick={this.decrease} >-</button>
-                <a-scene embedded arjs='source type:webcam;' >
-                   <a-assets>
-                        <a-asset-item id="tshirt-obj" src={shirtObj}></a-asset-item>
-                        <a-asset-item id="tshirt-mtl" src={shirtMtl}></a-asset-item>
-                    </a-assets>
+            <Fragment>
+                <div className="shoppingWorldAr">
+                    <a href="/Men" exact="true">
+                        <button className="arButtonScreen" id="arBackBtn" >Back</button>
+                    </a>
+                    <button className="arButtonScreen" id="arSizeIncrement" onClick={this.increase}>+</button>
+                    <button className="arButtonScreen" id="arSizeDecrement" onClick={this.decrease} >-</button>
+                    <a-scene embedded arjs='source type:webcam;' >
+                        <a-assets>
+                            <a-asset-item id="tshirt-obj" src={shirtObj}></a-asset-item>
+                            <a-asset-item id="tshirt-mtl" src={shirtMtl}></a-asset-item>
+                        </a-assets>
 
-                    <a-marker preset='hiro'>
-                        <a-entity id="arObject"  position="0 0 -1" rotation="270 0 0" scale=" 0.5 0.5 0.5"
-                        obj-model="obj:#tshirt-obj; mtl:#tshirt-mtl" >
-                             <a-animation id="objAnimation" attribute="rotation" dur="5000" fill="forwards" to="0 360 0"
-                              position="1 2 3" repeat="indefinite" begin="rotate" end="endrotate">
-                            </a-animation> 
-                      </a-entity>
-                    </a-marker>
-                </a-scene>
-            </div>
-        </Fragment>
+                        <a-marker preset='hiro'>
+                            <a-entity id="arObject" position="0 0 -1" rotation="270 0 0" scale=" 0.5 0.5 0.5"
+                                obj-model="obj:##tshirt-obj; mtl:##tshirt-mtl" >
+                                <a-animation id="objAnimation" attribute="rotation" dur="5000" fill="forwards" to="0 360 0"
+                                    position="1 2 3" repeat="indefinite" begin="rotate" end="endrotate">
+                                </a-animation>
+                            </a-entity>
+                        </a-marker>
+                    </a-scene>
+                </div>
+            </Fragment>
         )
     }
 }
-     export default Areality;
+export default Areality;
 
