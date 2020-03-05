e93e52cc4af3441b35699b38e3527b3371377e40
Nikhil Hegde <nikhil.hegde@ymedialabs.com>
2020-03-05 16:46:18 +0530

    Env file updates with environment variables

--- src/Config/Fire.js
+++ src/Config/Fire.js
@@ -3,14 +3,15 @@ import '@firebase/database';
 import '@firebase/auth';
 
 const config = { /* COPY THE ACTUAL CONFIG FROM FIREBASE CONSOLE */
-  apiKey: "AIzaSyBbQTt2hPhpk7ilOMIk5_ggg6AHRqpyW8o",
-  authDomain: "fashionar-f9e8b.firebaseapp.com",
-  databaseURL: "https://fashionar-f9e8b.firebaseio.com",
-  projectId: "fashionar-f9e8b",
-  storageBucket: "fashionar-f9e8b.appspot.com",
-  messagingSenderId: "127417700055",
-  appId: "1115190675295031",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
+
 export const fire = firebase.initializeApp(config);
 export const ref = firebase.database().ref();
 export const auth = firebase.auth;
